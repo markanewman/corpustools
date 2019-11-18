8bf53411b4bde3e25b69c3387fa2935be1ba9f65
Mark Newman <mark@trinetteandmark.com>
2019-11-18 18:42:55 -0500

    rename

--- measures/__init__.py
+++
@@ -1,5 +0,0 @@
-from .coverage import coverage
-from .frequency_counts import frequency_counts
-from .MATTR import MATTR
-from .relitive_frequency_ratio import relitive_frequency_ratio
-from .TTR import TTR
