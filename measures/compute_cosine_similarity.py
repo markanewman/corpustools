4ede3938a80f57a5bc5539d23090a9ec82ee523d
Mark Newman <mark@trinetteandmark.com>
2019-12-04 08:28:20 -0500

    fix empty line calculation

--- measures/compute_cosine_similarity.py
+++ measures/compute_cosine_similarity.py
@@ -52,29 +52,31 @@ def process_all_files(folder_in, measure_out):
     measure_out = pathlib.Path(measure_out)
 
     i = 0
-    widgets = [ 'Processing File #', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
+    widgets = [ 'Processing File # ', pb.Counter(), ' ', pb.Timer(), ' ', pb.BouncingBar(marker = '.', left = '[', right = ']')]
     with pb.ProgressBar(widgets = widgets) as bar:
         with measure_out.open('w', encoding = 'utf-8', newline='') as measure_out:
             writer = csv.writer(measure_out, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_ALL)
             writer.writerow(['filename', 'mean', 'sd'])
-            for file_name in folder_in.iterdir() if file_name.is_file():
-                bar.update(i)
-                i = i + 1
+            for file_name in folder_in.iterdir():
+                 if file_name.is_file():
+                    bar.update(i)
+                    i = i + 1
 
-                file_in = file_name
-                with file_in.open('r', encoding = 'utf-8') as file_in:
-                    lines = [line.strip().split() for line in file_in.readlines()]
-                
-                idf = compute_idf(lines)
-                result = compute_cosine_similarity(lines,  idf)
-                result_mean = stats.mean(result)
-                result_sd = stats.stdev(result)
-                writer.writerow([file_name.name, result_mean, result_sd])
+                    file_in = file_name
+                    with file_in.open('r', encoding = 'utf-8') as file_in:
+                        lines = [line.strip().split() for line in file_in.readlines()]                    
+                        lines = [line for line in lines if len(line) > 0]
+                    
+                    idf = compute_idf(lines)
+                    result = compute_cosine_similarity(lines,  idf)
+                    result_mean = stats.mean(result)
+                    result_sd = stats.stdev(result)
+                    writer.writerow([file_name.name, result_mean, result_sd])
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-in', '--folder-in', help = 'Folder to import files from', required = False)
-    parser.add_argument('-out', '--measure-out', help = 'File used to save the measures', required = False)
+    parser.add_argument('-in', '--folder-in', help = 'Folder to import files from', required = True)
+    parser.add_argument('-out', '--measure-out', help = 'File used to save the measures', required = True)
     args = parser.parse_args()
     print(f'folder in: {args.folder_in}')
     print(f'measure out: {args.measure_out}')
